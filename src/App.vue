ee99874ee9e37b6c8264f2df7a5fff1b95dd19e2
zhu-hong <zhxhyy@qq.com>
2022-03-19 11:03:33 +0800

    SVG版本

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
 let [
   elWidth,
   elHeight,
@@ -14,8 +16,15 @@ let [
 // 被拖动元素的宽高/元素的坐标/鼠标的坐标/鼠标距元素上下左右的坐标
 
 let inDrag: boolean = false
-const windowWidth = window.innerWidth
-const windowHeight = window.innerHeight
+let wrapperInfo: DOMRect
+
+onMounted(() => {
+  wrapperInfo = document.querySelector('svg')!.getBoundingClientRect()
+})
+
+window.addEventListener('resize', () => {
+  wrapperInfo = document.querySelector('svg')!.getBoundingClientRect()
+})
 
 const handleMouseDown = (e: MouseEvent): void => {
   const el: HTMLElement = (e.target as HTMLElement)
@@ -44,8 +53,8 @@ const handleMouseMove = (e: MouseEvent): void => {
   const moveX = e.clientX
   const moveY = e.clientY
 
-  elX = moveX - distanceLeft
-  elY = moveY - distanceTop
+  elX = moveX - distanceLeft - wrapperInfo.x
+  elY = moveY - distanceTop -wrapperInfo.y
 
   if (elX <= 0) {
     elX = 0
@@ -57,13 +66,13 @@ const handleMouseMove = (e: MouseEvent): void => {
   }
   // 上边界
 
-  if (moveX + distanceRight > windowWidth) {
-    elX = windowWidth - elWidth
+  if (moveX + distanceRight > wrapperInfo.width + wrapperInfo.left) {
+    elX = wrapperInfo.width - elWidth
   }
   // 右边界
 
-  if (moveY + distanceBottom > windowHeight) {
-    elY = windowHeight - elHeight
+  if (moveY + distanceBottom > wrapperInfo.height + wrapperInfo.y) {
+    elY = wrapperInfo.height - elHeight
   }
   // 下边界
 
@@ -74,26 +83,57 @@ const handleMouseUp = (e: MouseEvent): void => {
 }
 
 const setCoordinate = (x: number, y: number, el: HTMLElement): void => {
-  el.style.top = `${y}px`
-  el.style.left = `${x}px`
+  el.setAttribute('transform', `translate(${x},${y})`)
 }
 </script>
 
 <template>
-  <main bg="[#050505]" w="screen" h="screen" overflow="hidden" relative="~" flex="~" justify="between">
-    <div
-      v-for="e of 5"
-      absolute="~"
-      w="200px"
-      h="200px"
-      bg="dark-400"
-      hover="bg-dark-600"
-      active="bg-dark-800 cursor-move"
-      transition="colors"
-      @mousedown="handleMouseDown"
-      @mousemove="handleMouseMove"
-      @mouseup="handleMouseUp"
-      @mouseout="handleMouseUp"
-    ></div>
+  <main
+    bg="[#050505]"
+    w="screen"
+    h="screen"
+    overflow="hidden"
+    relative="~"
+    flex="~"
+    justify="center"
+  >
+    <div border="~ light-700" h="300px">
+      <svg
+        width="400"
+        height="300"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 400 300"
+      >
+        <rect
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          fill="#456"
+          @mousedown="handleMouseDown"
+          @mousemove="handleMouseMove"
+          @mouseup="handleMouseUp"
+          @mouseout="handleMouseUp"
+          active="cursor-move"
+        />
+        <text fill="#FFFFFF" font-size="16" x="100" y="100">
+          <tspan>资产编号是彩色打印机</tspan>
+        </text>
+        <rect
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          fill="#123"
+          @mousedown="handleMouseDown"
+          @mousemove="handleMouseMove"
+          @mouseup="handleMouseUp"
+          @mouseout="handleMouseUp"
+          active="cursor-move"
+        />
+      </svg>
+    </div>
   </main>
 </template>
